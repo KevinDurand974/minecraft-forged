409b3abb4200e27568e1ae08083bf182bdaaa1e1
KDurand974 <k.durand.974@gmail.com>
2022-08-31 19:40:39 +0200

    Add Custom React SVG icons

---
+++ components/icons/index.ts
@@ -0,0 +1,6 @@
+import ArrowBoldDown from "./ArrowBoldDown";
+import ArrowCurvedDown from "./ArrowCurvedDown";
+import Close from "./Close";
+import Logo from "./Logo";
+
+export { ArrowBoldDown, ArrowCurvedDown, Close, Logo };
